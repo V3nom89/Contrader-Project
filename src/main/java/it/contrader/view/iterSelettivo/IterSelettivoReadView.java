d64812202cdfd2b8718ad174fe4809627c918a1d
Nicolò Paolangeli <78351849+NicoloPaolangeli@users.noreply.github.com>
2021-02-15 15:53:37 +0100

    Update IterSelettivoReadView.java

--- src/main/java/it/contrader/view/iterSelettivo/IterSelettivoReadView.java
+++ src/main/java/it/contrader/view/iterSelettivo/IterSelettivoReadView.java
@@ -1,6 +1,7 @@
 package it.contrader.view.iterSelettivo;
 
 import it.contrader.controller.Request;
+import it.contrader.dao.IterSelettivoDAO;
 import it.contrader.dto.IterSelettivoDTO;
 import it.contrader.main.MainDispatcher;
 import it.contrader.view.AbstractView;
@@ -15,8 +16,8 @@ public class IterSelettivoReadView extends AbstractView {
 	}
 	
 	/*
-	 * Se la request è null (ovvero quando arriva dal controller con mode GETCHOICE e choice L 
-	 * il metodo è vuoto.
+	 * Se la request Ã¨ null (ovvero quando arriva dal controller con mode GETCHOICE e choice L 
+	 * il metodo Ã¨ vuoto.
 	 * 
 	 * Altrimenti se arriva con uno user nella request (ovvero quando arriva
 	 * dal controller con mode READ) mostra lo user. In questo caso torna alla UserView senza eseguire
@@ -46,7 +47,7 @@ public class IterSelettivoReadView extends AbstractView {
 	@Override
 	public void submit() {
 		request = new Request();
-		request.put("idIterSelettivo", idIterSelettivo);
+		request.put(IterSelettivoDAO.CONST.ID_ITER_SELETTIVO, idIterSelettivo);
 		request.put("mode", mode);
 		MainDispatcher.getInstance().callAction("IterSelettivo", "doControl", request);
 	}
