fc73a0d24d64a7ee213f59d1184d04624f2a789c
Alexis02031990 <57899361+Alexis02031990@users.noreply.github.com>
2021-02-16 16:06:30 +0100

    Update CorsoConverter.java

--- src/main/java/it/contrader/converter/CorsoConverter.java
+++ src/main/java/it/contrader/converter/CorsoConverter.java
@@ -2,7 +2,6 @@ package it.contrader.converter;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import it.contrader.dto.CorsoDTO;
 import it.contrader.model.Corso;
 
@@ -14,7 +13,7 @@ public class CorsoConverter {
 	 * Notare l'uso del metodo get() per ottenere il valore dell'attributo-
 	 */
 	public CorsoDTO toDTO(Corso corso) {
-		CorsoDTO corsoDTO = new CorsoDTO(corso.getIdCandidato(), corso.getIdIterSelettivo(), corso.getArgomentoCorso(),corso.getValutazioneComunicazioni(), corso.getValutazioneIntuitivita(), corso.getValutazioneAttitudine(), corso.getValutazioneTeamWork(), corso.getOreTotali(), corso.getIdStaff(), corso.getDataInizio());
+		CorsoDTO corsoDTO = new CorsoDTO(corso.getIdCorso(),corso.getIdCandidato(), corso.getIdIterSelettivo(), corso.getArgomentoCorso(),corso.getValutazioneComunicazioni(), corso.getValutazioneIntuitivita(), corso.getValutazioneAttitudine(), corso.getValutazioneTeamWork(), corso.getOreTotali(), corso.getIdStaff(), corso.getDataInizio());
 		return corsoDTO;
 	}
 
@@ -23,19 +22,19 @@ public class CorsoConverter {
 	 * Notare l'uso del metodo get() per ottenere il valore dell'attributo-
 	 */
 	public Corso toEntity(CorsoDTO corsoDTO) {
-		Corso corso = new Corso(corsoDTO.getIdCorso(), corsoDTO.getIdCandidato(), corsoDTO.getArgomentoCorso(),corsoDTO.getValutazioneComunicazioni(), corsoDTO.getValutazioneIntuitivita(), corsoDTO.getValutazioneAttitudine(), corsoDTO.getValutazioneTeamWork(), corsoDTO.getOreTotali(), corsoDTO.getIdStaff(), corsoDTO.getDataInizio());
+		Corso corso = new Corso(corsoDTO.getIdCorso(),corsoDTO.getIdCandidato(),corsoDTO.getIdIterSelettivo(), corsoDTO.getArgomentoCorso(),corsoDTO.getValutazioneComunicazioni(), corsoDTO.getValutazioneIntuitivita(), corsoDTO.getValutazioneAttitudine(), corsoDTO.getValutazioneTeamWork(), corsoDTO.getOreTotali(), corsoDTO.getIdStaff(), corsoDTO.getDataInizio());
 		return corso;
 	}
 	
 	/**
 	 * Metodo per convertire le liste di Corso.
 	 */
-	public List<CorsoDTO> toDTOList(List<Corso> corsoList) {
+	public List<CorsoDTO> toDTOList(List<Corso> List) {
 		//Crea una lista vuota.
 		List<CorsoDTO> corsoDTOList = new ArrayList<CorsoDTO>();
 		
 		//Cicla tutti gli elementi della lista e li converte uno a uno
-		for(Corso corso : corsoList) {
+		for(Corso corso : List) {
 			//Utilizza il metodo toDTO per convertire ogni singolo elemento della lista
 			//e lo aggiunge adda lista di DTO
 			corsoDTOList.add(toDTO(corso));
@@ -43,3 +42,4 @@ public class CorsoConverter {
 		return corsoDTOList;
 	}
 }
+
