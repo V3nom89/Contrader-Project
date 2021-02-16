6b728102fa0a984bad382c987423e3bbedc68c51
Alexis02031990 <57899361+Alexis02031990@users.noreply.github.com>
2021-02-16 13:31:19 +0100

    Update CorsoController.java

--- src/main/java/it/contrader/controller/CorsoController.java
+++ src/main/java/it/contrader/controller/CorsoController.java
@@ -7,9 +7,13 @@ import it.contrader.dto.CorsoDTO;
 import it.contrader.main.MainDispatcher;
 import it.contrader.service.CorsoService;
 
-
-
-public class CorsoController implements Controller{
+/**
+ * 
+ * @author Vittorio
+ *
+ *Si osservi che nel Controller compaiono solo oggetti del DTO e non del Model!
+ */
+public class CorsoController implements Controller {
 
 	/**
 	 * definisce il pacchetto di vista user.
@@ -28,10 +32,10 @@ public class CorsoController implements Controller{
 	
 	/**
 	 * Metodo dell'interfaccia Controller. Estrae dalla request la mode
-	 * (che riceve dalle view specifiche e può essere la richesta di una 
+	 * (che riceve dalle view specifiche e puï¿½ essere la richesta di una 
 	 * scelta da parte dell'utente "GETCHOICE") e la scelta dell'utente.
 	 * 
-	 * Se la modalità corrisponde ad una CRUD il controller chiama i service,
+	 * Se la modalitï¿½ corrisponde ad una CRUD il controller chiama i service,
 	 * altrimenti rimanda alla View della CRUD per richiedere i parametri
 	 */
 	@Override
@@ -43,43 +47,61 @@ public class CorsoController implements Controller{
 		String choice = (String) request.get("choice");
 
 		//Definisce i campi della classe (serviranno sempre, tanto vale definirli una sola volta)
-		int idCorso, idIterSelettivo,valutazioneComunicazioni,valutazioneIntuitivita,valutazioneAttitudine,valutazioneTeamWork,oreTotali,idCandidato,idStaff;
-		String argomentoCorso, dataInizio;
+		 int idCorso=0;
+		 int idCandidato;
+		 int idIterSelettivo = 0;
+	     String argomentoCorso;
+		 int valutazioneComunicazioni;
+		 int valutazioneIntuitivita;
+		 int valutazioneAttitudine;
+		 int valutazioneTeamWork;
+		 int oreTotali;
+		 int idStaff;
+		 String dataInizio;
 
 		switch (mode) {
 		
 		// Arriva qui dalla UserReadView. Invoca il Service con il parametro id e invia alla UserReadView uno user da mostrare 
 		case "READ":
-			idCorso = Integer.parseInt(request.get("idCorso").toString());
-			CorsoDTO corsoDTO = corsoService.read(idCorso);
-			request.put("corso", corsoDTO);
-			MainDispatcher.getInstance().callView(sub_package + "CorsoRead", request);
+			try {
+				idCorso = Integer.parseInt(request.get(CorsoDAO.CONST.ID_CORSO).toString());
+				CorsoDTO corsoDto = corsoService.read(idCorso);
+				request.put("corso", corsoDto);
+				MainDispatcher.getInstance().callView(sub_package + "CorsoRead", request);
+			} catch (Exception e) {
+				System.err.println("Errore nella lettura del membro dello Corso con id <" + idCorso + ">");
+				e.printStackTrace();
+			}
 			break;
 		
-		// Arriva qui dalla UserInsertView. Estrae i parametri da inserire e chiama il service per inserire uno user con questi parametri
+		// Arriva qui dalla UserInsertView. Estrae i parametri da inserire e chiama il service per inserire uno user con questi parametri 
+	        //aggiornato la Insert
 		case "INSERT":
-			idCorso = Integer.parseInt(request.get(CorsoDAO.CONST.ID_CORSO).toString());
-			idCandidato = Integer.parseInt(request.get(CorsoDAO.CONST.ID_CANDIDATO).toString());
-			idIterSelettivo = Integer.parseInt(request.get(CorsoDAO.CONST.ID_ITERSELETTIVO).toString());
-			argomentoCorso = request.get(CorsoDAO.CONST.ARGOMENTO_CORSO).toString();
-			valutazioneComunicazioni = Integer.parseInt(request.get(CorsoDAO.CONST.VALUTAZIONE_COMUNICAZIONI).toString());
-			valutazioneIntuitivita = Integer.parseInt(request.get(CorsoDAO.CONST.VALUTAZIONE_INTUITIVITA).toString());
-			valutazioneAttitudine = Integer.parseInt(request.get(CorsoDAO.CONST.VALUTAZIONE_ATTITUDINE).toString());
-			valutazioneTeamWork  = Integer.parseInt(request.get(CorsoDAO.CONST.VALUTAZIONE_TEAMWORK).toString());
-			oreTotali = Integer.parseInt(request.get(CorsoDAO.CONST.ORETOTALI).toString());
-			
-			idStaff = Integer.parseInt(request.get(CorsoDAO.CONST.ID_STAFF).toString());
-			dataInizio = request.get(CorsoDAO.CONST.DATA_INIZIO).toString();
-
-		
-			//costruisce l'oggetto user da inserire
-			CorsoDTO corsotoinsert = new CorsoDTO(idCorso, idCandidato, idIterSelettivo, argomentoCorso, valutazioneComunicazioni, valutazioneIntuitivita, valutazioneAttitudine, valutazioneTeamWork, oreTotali, idStaff, dataInizio);
-			//invoca il service
-			corsoService.insert(corsotoinsert);
-			request = new Request();
-			request.put("mode", "mode");
-			//Rimanda alla view con la risposta
-			MainDispatcher.getInstance().callView(sub_package + "CorsoInsert", request);
+			try {
+				
+				idCandidato= Integer.parseInt(request.get(CorsoDAO.CONST.ID_CANDIDATO).toString());
+				idIterSelettivo= Integer.parseInt(request.get(CorsoDAO.CONST.ID_ITERSELETTIVO).toString());
+				argomentoCorso= request.get(CorsoDAO.CONST.ARGOMENTO_CORSO).toString();
+				valutazioneComunicazioni= Integer.parseInt(request.get(CorsoDAO.CONST.VALUTAZIONE_COMUNICAZIONI).toString());
+				valutazioneIntuitivita= Integer.parseInt(request.get(CorsoDAO.CONST.VALUTAZIONE_INTUITIVITA).toString());
+				valutazioneAttitudine= Integer.parseInt(request.get(CorsoDAO.CONST.VALUTAZIONE_ATTITUDINE).toString());
+			        valutazioneTeamWork= Integer.parseInt(request.get(CorsoDAO.CONST.VALUTAZIONE_TEAMWORK).toString());
+				oreTotali= Integer.parseInt(request.get(CorsoDAO.CONST.ORETOTALI).toString());
+				idStaff= Integer.parseInt(request.get(CorsoDAO.CONST.ID_STAFF).toString());
+				dataInizio= request.get(CorsoDAO.CONST.DATA_INIZIO).toString();
+				
+				//costruisce l'oggetto user da inserire
+				CorsoDTO corsotoinsert = new CorsoDTO(idCorso,idCandidato,idIterSelettivo,argomentoCorso,valutazioneComunicazioni,valutazioneIntuitivita,valutazioneAttitudine,valutazioneTeamWork,oreTotali,idStaff,dataInizio);
+				//invoca il service
+				corsoService.insert(corsotoinsert);
+				request = new Request();
+				request.put("mode", "mode");
+				//Rimanda alla view con la risposta
+				MainDispatcher.getInstance().callView(sub_package + "CorsoInsert", request);
+			} catch (Exception e) {
+				System.err.println("Errore nell'inserimento di un componente dello corso");
+				e.printStackTrace();
+			}
 			break;
 		
 		// Arriva qui dalla UserDeleteView. Estrae l'id dell'utente da cancellare e lo passa al Service
@@ -92,24 +114,28 @@ public class CorsoController implements Controller{
 			MainDispatcher.getInstance().callView(sub_package + "CorsoDelete", request);
 			break;
 		
-		// Arriva qui dalla UserUpdateView
+		//Aggiornato la update
 		case "UPDATE":
-			idCorso = Integer.parseInt(request.get("idCorso").toString());
-			idIterSelettivo = Integer.parseInt(request.get("idIterSelettivo").toString());
-			argomentoCorso = request.get("argomentoCorso").toString();
-			valutazioneComunicazioni = Integer.parseInt(request.get("valutazioneComunicazioni").toString());
-			valutazioneIntuitivita = Integer.parseInt(request.get("valutazioneIntuitivita").toString());
-			valutazioneAttitudine = Integer.parseInt(request.get("valutazioneAttitudine").toString());
-			valutazioneTeamWork  = Integer.parseInt(request.get("valutazioneTeamWork").toString());
-			oreTotali = Integer.parseInt(request.get("oreTotali").toString());
-			idCandidato = Integer.parseInt(request.get("idCandidato").toString());
-			idStaff = Integer.parseInt(request.get("idStaff").toString());
-			dataInizio = request.get("dataInizio").toString();
-
+			idCorso= Integer.parseInt(request.get(CorsoDAO.CONST.ID_CORSO).toString());
+			idCandidato= Integer.parseInt(request.get(CorsoDAO.CONST.ID_CANDIDATO).toString());
+			idIterSelettivo= Integer.parseInt(request.get(CorsoDAO.CONST.ID_ITERSELETTIVO).toString());
+			argomentoCorso= request.get(CorsoDAO.CONST.ARGOMENTO_CORSO).toString();
+			valutazioneComunicazioni= Integer.parseInt(request.get(CorsoDAO.CONST.VALUTAZIONE_COMUNICAZIONI).toString());
+			valutazioneIntuitivita= Integer.parseInt(request.get(CorsoDAO.CONST.VALUTAZIONE_INTUITIVITA).toString());
+			valutazioneAttitudine= Integer.parseInt(request.get(CorsoDAO.CONST.VALUTAZIONE_ATTITUDINE).toString());
+		        valutazioneTeamWork= Integer.parseInt(request.get(CorsoDAO.CONST.VALUTAZIONE_TEAMWORK).toString());
+			oreTotali= Integer.parseInt(request.get(CorsoDAO.CONST.ORETOTALI).toString());
+			idStaff= Integer.parseInt(request.get(CorsoDAO.CONST.ID_STAFF).toString());
+			dataInizio= request.get(CorsoDAO.CONST.DATA_INIZIO).toString();
 			
-			CorsoDTO corsotoupdate = new CorsoDTO(idCorso,idCandidato, idIterSelettivo, argomentoCorso, valutazioneComunicazioni, valutazioneIntuitivita, valutazioneAttitudine, valutazioneTeamWork, oreTotali, idStaff, dataInizio);
+			//costruisce l'oggetto user da inserire
+			CorsoDTO corsotoupdate = new CorsoDTO(idCorso,idCandidato,idIterSelettivo,argomentoCorso,valutazioneComunicazioni,valutazioneIntuitivita,valutazioneAttitudine,valutazioneTeamWork,oreTotali,idStaff,dataInizio);
 			corsotoupdate.setIdCorso(idCorso);
-			corsoService.update(corsotoupdate);
+                        corsoService.update(corsotoupdate);
+                        //invoca il service
+			
+			
+
 			request = new Request();
 			request.put("mode", "mode");
 			MainDispatcher.getInstance().callView(sub_package + "CorsoUpdate", request);
@@ -117,10 +143,15 @@ public class CorsoController implements Controller{
 			
 		//Arriva qui dalla UserView Invoca il Service e invia alla UserView il risultato da mostrare 
 		case "CORSOLIST":
-			List<CorsoDTO> corsoDTO1 = corsoService.getAll();
-			//Impacchetta la request con la lista degli user
-			request.put("corso", corsoDTO1);
-			MainDispatcher.getInstance().callView("Corso", request);
+			try {
+				List<CorsoDTO> corso = corsoService.getAll();
+				//Impacchetta la request con la lista degli user
+				request.put("corso", corso);
+				MainDispatcher.getInstance().callView("Corso", request);
+			} catch (Exception e) {
+				System.err.println("Errore nella lettura della lista di membri dello Corso");
+				e.printStackTrace();
+			}
 			break;
 			
 		//Esegue uno switch sulla base del comando inserito dall'utente e reindirizza tramite il Dispatcher alla View specifica per ogni operazione
